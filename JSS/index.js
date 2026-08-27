f49ff25030e4e9936303230a41175c2f920adcf9
Sulok Pandey <118430934+Sulokp@users.noreply.github.com>
2026-08-27 14:53:01 +0545

    Delete JSS directory

--- JSS/index.js
+++
@@ -1,7 +0,0 @@
-alert("Warning you might be captivated by the contents.");
-const downloadCV = document.querySelector('a[download]');
-const downloadText = document.querySelector('#downloadText');
-
-downloadCV.addEventListener('click', function() {
-   downloadText.textContent = "Thank you";
-  });
